fb3e8b25e1c0d3660e5a4ccdde0e30bf35f6ad34
Siddhardha Tagirisa <siddhardhatagirisa@gmail.com>
2019-12-07 15:45:14 +0800

    add constants for unit tests

---
+++ src/test/java/com/drawingprogram/app/Constants.java
@@ -0,0 +1,19 @@
+package com.drawingprogram.app;
+
+public final class Constants {
+
+  private Constants() {
+    // restrict instantiation
+  }
+
+  public static String EMPTY_CANVAS204 = "----------------------\n|                    |\n|                    |\n|                    |\n|                    |\n----------------------\n";
+
+  public static String EMPTY_CANVAS35 = "-----\n|   |\n|   |\n|   |\n|   |\n|   |\n-----\n";
+
+  public static String LINE1214 = "-----\n|   |\n|X  |\n|X  |\n|X  |\n|   |\n-----\n";
+
+  public static String RECTANGLE141183 = "----------------------\n|             XXXXX  |\n|             X   X  |\n|             XXXXX  |\n|                    |\n----------------------\n";
+
+  public static String FILL103 = "----------------------\n" + "|oooooooooooooooooooo|\n" + "|oooooooooooooooooooo|\n"
+      + "|oooooooooooooooooooo|\n" + "|oooooooooooooooooooo|\n" + "----------------------\n";
+}
